7767f122e570a178b982134dca7a5aacaf019b4e
Walassi Silva <walassi.b@gmail.com>
2024-10-12 15:06:30 -0300

    chore: add and config `tailwindcss`

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 
-import './App.css'
 
 function App() {
 
   return (
     <>
-      <main>
+      <main className="text-red-500">
         <h1>Seja bem vindo! :)</h1>
         <p>Desafio de apresentação Pessoal do Projeto Frontend fusion</p>
       </main>
